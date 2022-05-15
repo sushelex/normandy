88c1569260cc938cdb2019fd56d9dd8a804a4125
sushil kumar gupta <sushil@sushils-MacBook-Air.local>
2022-05-15 14:28:12 +0530

    3

--- src/test/java/com/pure/roxy/normandy/HomePage.java
+++ src/test/java/com/pure/roxy/normandy/HomePage.java
@@ -1,10 +1,12 @@
 package com.pure.roxy.normandy;
 
+import org.openqa.selenium.By;
+
 public class HomePage {
 
 public void clickOnHomeIcon()
 {
-  Base.getDriver().findElement(By.xpath("")).click(); 
+  Base.getdriver().findElement(By.xpath("")).click(); 
 }
 
 }
